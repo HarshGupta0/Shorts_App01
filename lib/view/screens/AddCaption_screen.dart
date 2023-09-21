3a16fedc772f1c89014588a38a433b6f277149ac
harshgupta0 <hg09032@gmail.com>
2023-09-21 18:23:19 +0530

    added caption screen

--- lib/view/screens/AddCaption_screen.dart
+++ lib/view/screens/AddCaption_screen.dart
@@ -1,45 +1,98 @@
 import 'dart:io';
+import 'package:shorts_app/constants.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
 class addCaption extends StatefulWidget {
   File videoFile;
   String videoPath;
 
- addCaption({Key? key,
-   required this.videoFile,
-   required this.videoPath,
- }) :super(key: key);
+  addCaption({
+    Key? key,
+    required this.videoFile,
+    required this.videoPath,
+  }) : super(key: key);
 
   @override
   State<addCaption> createState() => addCaptionState();
 }
 
 class addCaptionState extends State<addCaption> {
- late VideoPlayerController videoPlayerController;
+  late VideoPlayerController videoPlayerController;
+  TextEditingController SongNameController =new TextEditingController();
+  TextEditingController CaptionNameController =new TextEditingController();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     setState(() {
-      videoPlayerController =VideoPlayerController.file(widget.videoFile);
+      videoPlayerController = VideoPlayerController.file(widget.videoFile);
     });
     videoPlayerController.initialize();
     videoPlayerController.play();
     videoPlayerController.setLooping(true);
     videoPlayerController.setVolume(.7);
-
-
   }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: MediaQuery.of(context).size.height,
-          child:VideoPlayer(videoPlayerController),
-          )
-        ],
-      )
-    );
+        // resizeToAvoidBottomInset: false,
+        body: Container(
+          margin: EdgeInsets.all(30),
+          child: SingleChildScrollView(
+            child: Column(
+
+      children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.5,
+              child: VideoPlayer(videoPlayerController),
+            ),
+            SizedBox(height: 5,),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: CaptionNameController,
+                    keyboardType: TextInputType.multiline,
+                    minLines: 1,
+                    maxLines: 6,
+                    onTap: (){
+                    },
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      labelText:'Caption',
+                      icon: Icon(Icons.closed_caption),
+                      iconColor: Colors.red.shade50,
+                    ),
+                  ),
+                  SizedBox(
+                    height :15,
+                  ),
+                  TextField(
+                    controller: SongNameController,
+                    keyboardType: TextInputType.multiline,
+                    // keyboardAppearance:,
+                    onTap: (){},
+                    textInputAction: TextInputAction.done,
+                    minLines: 1,
+                    maxLines: 6,
+                    decoration: InputDecoration(
+                      labelText:'Song Name',
+                      icon: Icon(Icons.queue_music_sharp,color: Colors.red.shade50,),
+                      iconColor: Colors.red.shade50,
+                    ),
+                  ),
+                  SizedBox(height: 12,),
+                  ElevatedButton(onPressed:(){}, child:Text("Upload"),style: ElevatedButton.styleFrom(primary: buttonColor),),
+                  ],
+              ),
+            )
+      ],
+    ),
+          ),
+        ));
   }
 }
