4b36e1035d7da2e8ad63a8cebb6bccedeb4d90d1
harshgupta0 <hg09032@gmail.com>
2023-11-17 19:03:22 +0530

    created upload profile img and save data to firbase storage method

--- lib/Controller/auth_controller.dart
+++ lib/Controller/auth_controller.dart
@@ -29,7 +29,48 @@ class AuthenticationController extends GetxController
     }
     _pickedFile=Rx<File?>(File(pickedImageFile!.path));
   }
-  void createAccountForNewUser(File imageFile ,String userName , String userEmail,String userPassword,){
 
+  void createAccountForNewUser(File imageFile, String userName, String userEmail, String userPassword) async {
+    try {
+      // 1. Create user in Firebase Auth
+      UserCredential credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: userEmail,
+        password: userPassword,
+      );
+
+      // 2. Save the user profile image to Firebase Storage
+      String imagedownloadUrl = await _uploadProfilePhoto(credential.user!, imageFile);
+
+      // 3. Save user data to the Firestore database
+      MyUser user = MyUser(
+        name: userName,
+        email: userEmail,
+        uid: credential.user!.uid,
+        profilePhoto: imagedownloadUrl,
+      );
+
+      await FirebaseFirestore.instance.collection('users').doc(credential.user!.uid).set(user.toJson());
+      Get.snackbar("Account created ", " Welcome ");
+
+      // Optional: You may perform additional actions or UI updates after account creation.
+    } catch (e) {
+      // Handle any errors that occurred during the account creation process.
+      Get.snackbar("Account creation Failed", "Error Occured ");
+      print("Error creating account: $e");
+    }
   }
+
+  Future<String> _uploadProfilePhoto(User user, File imageFile) async {
+    try {
+      Reference ref = FirebaseStorage.instance.ref().child('ProfilePics').child(user.uid);
+      UploadTask uploadTask = ref.putFile(imageFile);
+      TaskSnapshot snapshot = await uploadTask;
+      return await snapshot.ref.getDownloadURL();
+    } catch (e) {
+      // Handle any errors that occurred during the file upload.
+      print("Error uploading profile photo: $e");
+      return ""; // You may want to handle this more gracefully in your application.
+    }
+  }
+
 }
